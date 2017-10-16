c30037c5a20ce460a6aa999f7a37d50a5356331b
GitJove <jovetrajkoski@gmail.com>
2017-10-16 17:22:05 +0200

    Nbrm Soap with express.js

---
+++ client.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var soap = require('soap');
+var bodyParser = require('body-parser');
+var app = express();
+
+require('body-parser-xml')(bodyParser);
+
+app.use(bodyParser.xml({
+    limit:'1MB',
+    xmlParseOptions:{
+        normalize:true,
+        normalizeTags:true,
+        explicitArray:false
+    }
+}));
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname + "/" + "/client.html");
+})
+
+app.post('/getAge',bodyParser.urlencoded({extended:false}),function(req,res){
+    console.log(req.body);
+    var input = req.body;
+    console.log(input.nbrmdata.startDate);
+    console.log(input.nbrmdata.endDate);
+
+    /* 
+    -beginning of soap body
+    -url is defined to point to server.js so that soap cient can consume soap server's remote service
+    -args supplied to remote service method
+    */
+    var url = "http://localhost:3030/nbrm?wsdl";
+
+    var args = {StartDate:'12.02.2010', EndDate:'15.02.2010'};
+    // var args = {StartDate:input.nbrmdata.startDate, EndDate:input.nbrmdata.endDate};
+    soap.createClient(url,function(err,client){
+        if(err)
+            console.error(err);
+        else {
+            client.GetExchangeRates(args,function(err,response){
+                if(err)
+                    console.error(err);
+                else{
+                    console.log(response);
+                    res.send(response);
+                }
+            })
+        }
+    });
+})
+
+var server = app.listen(3036,function(){
+    var host = "127.0.0.1";
+    var port = server.address().port;
+    console.log("server running at http://%s:%s\n",host,port);
+})
